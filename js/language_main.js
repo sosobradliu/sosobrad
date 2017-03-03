5aff94a4e71f18c87e07ef799382fdd311f9175c
sosobradliu <sosobrad@gmail.com>
2017-03-03 11:51:06 +0800

    first
    index & image

---
+++ js/language_main.js
@@ -0,0 +1,41 @@
+//all language
+
+//English
+L_EN = new Array();
+L_EN[0] = "Main";
+L_EN[1] = "Navigation";
+L_EN[2] = "Radio";
+L_EN[3] = "Bluetooth";
+L_EN[4] = "Link";
+L_EN[5] = "MultiMedia";
+L_EN[6] = "Settings";
+
+//taiwan
+L_TW = new Array();
+L_TW[0] = "主頁";
+L_TW[1] = "導航";
+L_TW[2] = "收音機";
+L_TW[3] = "藍芽電話";
+L_TW[4] = "雙屏互動";
+L_TW[5] = "多媒體";
+L_TW[6] = "設定";
+
+//china
+L_CN = new Array();
+L_CN[0] = "主页";
+L_CN[1] = "导航";
+L_CN[2] = "收音机";
+L_CN[3] = "蓝芽电话";
+L_CN[4] = "双屏互动";
+L_CN[5] = "多媒体";
+L_CN[6] = "设置";
+
+//japan
+L_JP = new Array();
+L_JP[0] = "ホームページ";
+L_JP[1] = "ナビゲーション";
+L_JP[2] = "ラジオ";
+L_JP[3] = "ブルートゥース";
+L_JP[4] = "リンク";
+L_JP[5] = "プレーヤー";
+L_JP[6] = "環境設定";
